7e4228d242d25b184762b4965b027a9346a02844
Seb <sebastian.strangert@gmail.com>
2024-03-24 20:32:50 +0100

    added door

--- scripts/delay.js
+++ scripts/delay.js
@@ -7,9 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
         var h3 = document.getElementById("fadeH3");
         h3.classList.add("fade-out");
 
+        setTimeout(function() {
+            var doorgif = document.getElementById("doorgif");
+            doorgif.src = "images/door_optimized.gif"; // Replace "yourGif.gif" with the path to your GIF
+        }, 2000); // Adjust the delay time as needed (1000 milliseconds = 1 second)
+
+        document.getElementById("doorgif").style.display = "inline"; // Show the GIF
+          
+
         // Add a delay before redirecting to main.html
         setTimeout(function() {
             window.location.href = "main.html"; // Replace with your desired URL
-        }, 2000); // Adjust the delay as needed
+        }, 7000); // Adjust the delay as needed
     });
 });

f5439eba8562b989bb40e9412a19d1e9e3235954
Sebastian <sebastian.strangert@gmail.com>
2024-03-25 15:04:10 +0100

    sidebar

--- scripts/delay.js
+++ scripts/delay.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+        var doorGIF = new Image();
+        doorGIF.src = "images/door_optimized.gif"; 
+
     document.getElementById("fadeOutDiv").addEventListener("click", function(event) {
         event.preventDefault(); // Prevent the default action of the click event
         
@@ -10,9 +14,10 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(function() {
             var doorgif = document.getElementById("doorgif");
             doorgif.src = "images/door_optimized.gif"; // Replace "yourGif.gif" with the path to your GIF
-        }, 2000); // Adjust the delay time as needed (1000 milliseconds = 1 second)
+            document.getElementById("doorgif").style.display = "inline"; // Show the GIF
+        }, 1000); // Adjust the delay time as needed (1000 milliseconds = 1 second)
 
-        document.getElementById("doorgif").style.display = "inline"; // Show the GIF
+
           
 
         // Add a delay before redirecting to main.html
